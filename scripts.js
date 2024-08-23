6f511b0903d52c43620adea64aff73101c30c43a
Andrea <andreagelleee@gmail.com>
2024-08-23 14:46:49 +0800

    update

--- scripts.js
+++ scripts.js
@@ -4,11 +4,10 @@ async function getUserLocation() {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log('Country Code:', data.country);
-        return data.country; // Return country code (e.g., 'BR' for Brazil)
+        return data.country;
     } catch (error) {
         console.error('Error fetching location:', error);
-        return 'Unknown'; // Fallback in case of error
+        return 'Unknown';
     }
 }
 
@@ -17,24 +16,41 @@ async function redirectBasedOnLocation() {
     const currentLocation = window.location.href;
     const countryCode = await getUserLocation();
 
-    console.log('Current Location:', currentLocation);
-    console.log('Country Code:', countryCode);
-
-    // Define the URLs for redirection
+    // URLs for redirection
     const brazilUrl = 'https://5518win.com/index?type=4&channelId=7381&fb_dynamic_pixel=2774791946023763';
     const internationalUrl = 'https://digital-playhouse.onrender.com/';
 
-    // Prevent redirection loop by checking if already on the target page
-    if (countryCode === 'BR' && !currentLocation.includes('5518win.com')) {
-        console.log('Redirecting to Brazil URL:', brazilUrl);
-        window.location.replace(brazilUrl);
-    } else if (countryCode !== 'BR' && !currentLocation.includes('digital-playhouse.onrender.com')) {
-        console.log('Redirecting to International URL:', internationalUrl);
-        window.location.replace(internationalUrl);
-    } else {
-        console.log('No redirection needed');
+    // Check local storage to see if the user has already been redirected
+    const hasRedirected = localStorage.getItem('hasRedirected');
+
+    if (!hasRedirected) {
+        if (countryCode === 'BR' && !currentLocation.includes('5518win.com')) {
+            localStorage.setItem('hasRedirected', 'true');
+            window.location.replace(brazilUrl);
+        } else if (countryCode !== 'BR' && !currentLocation.includes('digital-playhouse.onrender.com')) {
+            localStorage.setItem('hasRedirected', 'true');
+            window.location.replace(internationalUrl);
+        }
     }
 }
 
 // Call the redirect function when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', redirectBasedOnLocation);
+
+// JavaScript for Modal
+const modal = document.getElementById('exampleModal');
+const closeModal = document.querySelector('.close-modal');
+
+document.querySelector('.interactive-button').addEventListener('click', function() {
+    modal.style.display = 'flex';
+});
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
